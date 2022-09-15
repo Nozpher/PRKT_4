722c70dea32daa1669c243e0331e40828c685bcc
Nozpher <66372864+Nozpher@users.noreply.github.com>
2022-09-15 08:24:09 +0500

    Update 3.c

--- 3.c
+++ 3.c
@@ -4,29 +4,33 @@
 
 const n = 528;
 int main()
-{   char A[n], word, *word2, alphg, sep[10] = " ";
-    int i, a,bk=0, words = 0, dlin = 0, sered = 0;
+{   char A[n], word,*word2, *alphg, sep[10] = " ", *last;
+    int i, a,bk=0, dlin = 0, sered = 0;
     printf("Write your string: ");
-    word = gets(A);
-    word2 = strtok(A, sep);
-    While (word2 != NULL){
+    gets(A);
+    word2 = strtok(A, " ");
+
+    while (word2!=NULL){
+
         dlin = strlen(word2);
         if (dlin % 2 != 0){
-            sered = (dlin-1) / 2;
-            strncpy(alphg, words, sered);
+            sered = ((dlin-1) / 2);;
+            alphg = word2[sered];
             for (a = 0; a < dlin; a ++)
-                if (A[a] == alphg){
+                if (word2[a] == alphg){
                         bk+=1;
-                } else if(bk >2){
+                } else if(bk > 2){
                     bk=0;
                     dlin=0;
-                } else{
-                    printf("%s ", words);
-                    bk=0;
-                    words = "";
-                    dlin=0;
-                    }
+                }
+            if (bk<2){
+                bk=0;
+            } else if (bk == 2){
+                printf("%s ", word2);
+                bk=0;
             }
-        }
+            }
+        word2 = strtok(0, " ");
+    }
     return 0;
 }
